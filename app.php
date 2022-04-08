15e028c8c4b4b6bc3dc693d0f7fb08226a9baf17
Elvis Gannem <egannem@gmail.com>
2022-04-08 17:49:05 -0300

    Command to create attributes

--- app.php
+++ app.php
@@ -5,26 +5,43 @@ function createDescription(): void
 {
     echo PHP_EOL . "--create [ARGUMENTS]:" . PHP_EOL;
     echo "      --file [PATH] [NAME] (Creates a file with .php extension)" . PHP_EOL;
-    echo "      --class [PATH] [NAME] (Creates a PHP class)" . PHP_EOL;
+    echo "      --class [PATH] [NAME](Creates a PHP class)" . PHP_EOL;
+    echo "          --attr:one,two,three (Creates private attributes - optional)";
 }
 
 function createFile(string $path, string $filename): void
 {
+    if(!file_exists($path)) {
+        mkdir($path, 0777, true);
+    }
     file_put_contents("$path/$filename.php", '<?php');
 }
 
-function createClass(string $path, string $filename): void
+function createClass(string $path, string $filename, string $attributes = ''): void
 {
-    file_put_contents("$path/$filename.php", classContent($filename));
+    if(!file_exists($path)) {
+        mkdir($path, 0777, true);
+    }
+    file_put_contents("$path/$filename.php", classContent($filename, $attributes));
 }
 
-function classContent(string $className): string
+function classContent(string $className, string $attributes): string
 {
     return "<?php" . PHP_EOL . "class $className " . PHP_EOL . "{
+    " . PHP_EOL . $attributes . "
     public function __construct()
     {
         //CONSTRUCT CONTENT
     } " . PHP_EOL . "}";
+}
+
+function createAttributes(array $attributes): string
+{
+    $attrs = '';
+    foreach($attributes as $attribute) {
+        $attrs .= "private $$attribute;" . PHP_EOL;
+    }
+    return $attrs;
 }
 
 if($argc === 1 || $argv[1] === '--help') {
@@ -38,8 +55,17 @@ if($argc === 1 || $argv[1] === '--help') {
             createFile($argv[3], $argv[4]);
             echo PHP_EOL . "\e[0;32mFile $argv[4].php created\e[0m" . PHP_EOL;
         } else if(isset($argv[2]) && $argv[2] === '--class' && isset($argv[4])) {
-            createClass($argv[3], $argv[4]);
+
+            if(isset($argv[5])) {
+                $attributes = explode(':', $argv[5]);
+                $attributes = explode(',', $attributes[1]);
+                createClass($argv[3], $argv[4], createAttributes($attributes));
+            } else {
+                createClass($argv[3], $argv[4]);
+            }
+
             echo PHP_EOL . "\e[0;32mClass $argv[4].php created\e[0m" . PHP_EOL;
+
         } else {
             createDescription();
         }
