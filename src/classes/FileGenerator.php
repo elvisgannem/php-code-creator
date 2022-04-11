1729bf6d6022647c68b09b13ba8b3fd1a102eb15
Elvis Gannem <egannem@gmail.com>
2022-04-11 17:27:09 -0300

    File and help classes

---
+++ src/classes/FileGenerator.php
@@ -0,0 +1,12 @@
+<?php
+
+class FileGenerator
+{
+    public static function createFile(string $path, string $filename): void
+    {
+        if(!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
+        file_put_contents("$path/$filename.php", '<?php');
+    }
+}
---
+++ src/classes/HelpGenerator.php
@@ -0,0 +1,19 @@
+<?php
+
+class HelpGenerator
+{
+    public static function createWelcomeMessage(): void
+    {
+        echo PHP_EOL . "\e[0;32mWelcome to PHP code creator command line helper.\e[0m" . PHP_EOL;
+        echo "If you have any problem or suggestion, please, be free to write an email: egannem@gmail.com" . PHP_EOL;
+        echo PHP_EOL . "\e[0;32mCommand that you can use: \e[0m" . PHP_EOL;
+    }
+
+    public static function createDescription(): void
+    {
+        echo PHP_EOL . "--create [ARGUMENTS]:" . PHP_EOL;
+        echo "      --file [PATH] [NAME] (Creates a file with .php extension)" . PHP_EOL;
+        echo "      --class [PATH] [NAME](Creates a PHP class)" . PHP_EOL;
+        echo "          --attr:one,two,three (Creates private attributes - optional)";
+    }
+}
